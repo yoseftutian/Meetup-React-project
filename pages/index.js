632e1c06c62a338fbdb3ee6244a853cfd9e1824f
yosef-tutian <yoseftutian@gmail.com>
2024-11-27 18:46:12 +0200

    create HomePage & NewMeetupPage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
+import MeetupList from "../components/meetups/MeetupList";
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a firsf meetup ",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup ",
+  },
+];
+
+
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
