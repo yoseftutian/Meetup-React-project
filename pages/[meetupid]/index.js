45df598b8ba51c46f9c8124674c495052887c66c
yosef-tutian <yoseftutian@gmail.com>
2024-12-01 21:18:14 +0200

    add MeetupDetail component

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="Firsf meetup"
+      address="Some Street 5, Some City"
+      description="This is a Firsf meetup"
+    />
+  );
+}
