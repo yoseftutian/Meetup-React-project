948abcef6bb8b10b2881371558ada6076fd8f4cc
yosef-tutian <yoseftutian@gmail.com>
2024-12-03 18:52:05 +0200

    Add static paths and props for meetup details page

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -10,3 +10,40 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid: "m1",
+        },
+      },
+      {
+        params: {
+          meetupid: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupid;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "Firsf meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a Firsf meetup",
+      },
+    },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,16 @@ export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
 export async function getStaticProps() {
   return {
     props: {
