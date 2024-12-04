7f79958b1af6350027bec114b609cb1acd4d124c
yosef-tutian <yoseftutian@gmail.com>
2024-12-04 18:48:21 +0200

    connect to mongodb and create a post request

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://yoseftutian:05484gps@shklimcluster.w6r058d.mongodb.net/meetups?retryWrites=true&w=majority&appName=ShklimCluster"
+    );
+    const db = client.db();
+
+    const meetupCollection = db.collection("meetups");
+    const result = await meetupCollection.insertOne({ data });
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
